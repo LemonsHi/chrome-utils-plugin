07cd8cbb1bc23b68710cb7831e46167af7b68e69
wujiawen009 <wujiawen009@ke.com>
2025-07-03 11:45:41 +0800

    fix: 删除一些注释

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,10 @@ const App: FC = () => {
   return (
     <ConfigProvider
       locale={zhCN}
-      /** 这里就是方案 3 的关键点 * */
       theme={{
         token: {
-          /** 主色想改都可以改；默认就是 #1677ff */
           colorPrimary: '#1677ff',
         },
-        /** 若想暗色模式时自动改变量，把 algorithm 换成 theme.darkAlgorithm */
         algorithm: theme.defaultAlgorithm,
       }}
     >
--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,3 @@
-// src/background.ts - MV3 Service Worker
 chrome.runtime.onInstalled.addListener(() => {
-  // 把“点击扩展图标”与 side panel 绑定
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(console.error);
 });
